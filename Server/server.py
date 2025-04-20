58764f32abe0daa48de2071dac58684d47946d41
Nicky9319 <paarthsaxena2005@gmail.com>
2025-04-20 15:32:37 +0530

    Added the Cors to the server

--- Server/server.py
+++ Server/server.py
@@ -3,8 +3,19 @@ from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 import uvicorn
 
+from fastapi.middleware.cors import CORSMiddleware
+
+
 # Initialize FastAPI app
 app = FastAPI(title="User Registration API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 # Setup MongoDB connection
 try:
